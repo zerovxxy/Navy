c9d88f483f921a8a030200be51ca95bdb21aa3b6
Alex <alexander.wen.fung@gmail.com>
2021-08-03 16:53:57 -0400

    js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
-//Animes
+//Animes... this might not be used acctually
 var fantasy
 var sliceOfLife
 var comedy
-var action
-var romance
 var shonin
 var shojo
-var horror
 var scifi
+// arrays
+var action = ["attack on titan, demon slayer, hunter x hunter, naruto, naruto shippuden, blue exorcist, dragonball, Familiar of zero, sao, yea im a spider so what"];
+var romance = ["horimiya, fruits basket, love is war, maid-sama, toradora, kimi ni todoke, orange, your name, a silent voice, my teen romantic comedy SNAFU"];
+var shoujoAi = ["high rise invasion"];
+var Shounen = ["naruto, naruto shippuden, hunter x hunter, code geass, bleach, black clover, jojos bizarre adventure, kill la kill, soul eater, fairy tale"];
+var pyschological = ["psycho pass, classroom of the elite, danganronpa, tower of god, wonder egg priority"];
+var adventure = [" gintama, hunter x hunter, fairy tale, my hero academia, naruto, naruto shippuden, bungo stray dogs"];
+var horror = ["promised neverland, blood-c, erased, death note"];
+
