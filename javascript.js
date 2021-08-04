f0dfa4b8480781c34487891bea81d6d211c7c3c1
Meira Pressner <pressnerm28@gmail.com>
2021-08-04 16:42:05 -0400

    add ShoujoAi

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ var scifi
 // arrays
 var action = ["attack on titan, demon slayer, hunter x hunter, naruto, naruto shippuden, blue exorcist, dragonball, Familiar of zero, sao, yea im a spider so what"];
 var romance = ["horimiya, fruits basket, love is war, maid-sama, toradora, kimi ni todoke, orange, your name, a silent voice, my teen romantic comedy SNAFU"];
-var shoujoAi = ["high rise invasion"];
+var shoujoAi = ["high rise invasion,angel beats, citrus"];
 var Shounen = ["naruto, naruto shippuden, hunter x hunter, code geass, bleach, black clover, jojos bizarre adventure, kill la kill, soul eater, fairy tale"];
 var pyschological = ["psycho pass, classroom of the elite, danganronpa, tower of god, wonder egg priority"];
 var adventure = [" gintama, hunter x hunter, fairy tale, my hero academia, naruto, naruto shippuden, bungo stray dogs"];
